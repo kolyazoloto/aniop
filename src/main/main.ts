28d452220524e3eaee2819d036dcf68d7d14938f
Nikolay Zolotarev <kolyazoloto@gmail.com>
2022-08-09 15:29:47 +0800

    start project

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,30 +1,30 @@
-import {app, BrowserWindow, ipcMain} from 'electron';
-import {join} from 'path';
+import { app, BrowserWindow, ipcMain } from "electron";
+import { join } from "path";
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
       nodeIntegration: false,
       contextIsolation: true,
-    }
+      webSecurity: false,
+    },
   });
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     const rendererPort = process.argv[2];
     mainWindow.loadURL(`http://localhost:${rendererPort}`);
-  }
-  else {
-    mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
+  } else {
+    mainWindow.loadFile(join(app.getAppPath(), "renderer", "index.html"));
   }
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -33,10 +33,10 @@ app.whenReady().then(() => {
   });
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
 
-ipcMain.on('message', (event, message) => {
+ipcMain.on("message", (event, message) => {
   console.log(message);
-})
+});
